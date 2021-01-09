e471a51159d74a9e3376a0dd7f4f63fec76c2f26
fukiran <macdorphoto@yahoo.com>
2021-01-09 00:33:26 +0000

    Append the submitted data to the list

---
+++ js/app.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const newPlayerForm = document.querySelector('#new-player-form');
+    newPlayerForm.addEventListener('submit', handleNewPlayerFormSubmit);
+
+    // const deleteAllButton = document.querySelector('#delete-all');
+    // deleteAllButton.addEventListener('click', handleDeleteClick);
+});
+
+const handleNewPlayerFormSubmit = function (event) {
+    event.preventDefault();
+    console.log(this)
+    const playersListItem = createPlayersListItem(event.target);
+    const playersList = document.querySelector('#players-list');
+    playersList.appendChild(playersListItem);
+
+    event.target.reset();
+};
+
+const createPlayersListItem = function (form) {
+    const playersListItem = document.createElement('li');
+    playersListItem.classList.add('players-list-item');
+
+    const name = document.createElement('h3');
+    name.textContent = form.name.value;
+    playersListItem.appendChild(name);
+
+    const faction = document.createElement('p');
+    faction.textContent = form.faction.value;
+    playersListItem.appendChild(faction);
+
+    const mat = document.createElement('p');
+    mat.textContent = form.mat.value;
+    playersListItem.appendChild(mat);
+
+
+    return playersListItem;
+};
+
+
+
